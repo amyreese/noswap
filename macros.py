d96e879fbc8a7f3b1f2d21b822efa2a971a0c79f
John Reese <jreese@leetcode.net>
2010-05-23 20:11:28 -0400

    Implemented mod_rewrite for pretty URL

--- macros.py
+++ macros.py
@@ -1,15 +1,41 @@
 
+import clevercss
+import glob
+import os
+from os import path
+import re
+
 page = {
 	"logo": "LeetCode.net",
 	"logo-url": "/",
 	"tagline": "open source software engineering",
 	}
 
-### CleverCSS
+### Pretty URLs
 
-import clevercss
-import glob
-import os.path
+prettyurl = re.compile("^(.+/)?([^/]+)\.html$")
+def pretty(url):
+	match = prettyurl.search(url)
+
+	if match:
+		file = match.group(2)
+
+		if match.group(1) is None:
+			if match.group(2) == "index":
+				return "/"
+			else:
+				return "/%s/" % (file)
+		else:
+			dir = match.group(1).rstrip("/")
+
+			if file == "index":
+				return "/%s/" % dir
+			else:
+				return "/%s/%s/" % (dir, file)
+	else:
+		return url
+
+### CleverCSS
 
 def once_clevercss():
 	print("Building CSS files...")
@@ -24,3 +50,9 @@ def once_clevercss():
 		fpi.close()
 		fpo.close()
 
+def once_htaccess():
+	fi = open(os.path.join(input, ".htaccess"))
+	fo = open(os.path.join(output, ".htaccess"), "w")
+	fo.writelines(fi.readlines())
+	fi.close()
+	fo.close()
